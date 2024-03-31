6990c17678bb6b84b487fd914943a73fcb9db148
anasyakubu <yakubuanas04@gmail.com>
2024-03-31 17:42:55 +0100

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+const App = () => {
+  return (
+    <>
+      <div className="text-center mt-5">
+        <Button>Add Button</Button>
+      </div>
+    </>
+  );
+};
+
+export default App;
